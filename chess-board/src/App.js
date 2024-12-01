9815dd05f9e2e28011ec21af7f8b260aef7d2364
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-12-01 21:12:26 +0100

    Component to join Fig picker and board, movement WIP

--- chess-board/src/App.js
+++ chess-board/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Board from './components/Board';
+import FigurePicker from './components/FigurePicker';
 
 function App() {
+
+  const [selectedFigure, setSelectedFigure] = useState(null);
+  const [selectedSquare, setSelectedSquare] = useState(null);
+
+  const handleFigurePicked = (figure) => {
+    // console.log(`Figure ${figure}`);
+    setSelectedFigure(figure);
+  }
+
+  const handleSquarePicked = (row, col) => {
+    if (selectedFigure != null) {
+      setSelectedSquare({ row, col });
+      console.log(`Row: ${row}, Col: ${col}, Figure: ${selectedFigure}`)
+      setSelectedFigure(null);
+      setSelectedSquare(null);
+    }
+  }
+
+  //Figure movement
+  //king
+  const kingMovement = [
+    [0, +1],
+    [0, -1],
+    [+1, 0],
+    [-1, 0],
+    [+1, +1],
+    [+1, -1],
+    [-1, +1],
+    [-1, -1],
+  ];
+
+  //rook
+  const rookMovement = [
+    [+1, +2],
+    [+1, -2],
+    [-1, +2],
+    [-1, -2],
+    [+2, +1],
+    [+2, -1],
+    [-2, +1],
+    [-2, -1]
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chess Learning</h1>
+      <div className="main-container">
+        <Board onClick={handleSquarePicked} />
+        <FigurePicker onClick={handleFigurePicked} />
+      </div>
     </div>
   );
 }
